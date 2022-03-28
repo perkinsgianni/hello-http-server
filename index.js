9dfbb672a0dbd867a31db9b46069d9221ce5acc8
Gianni P <perkinsgianni@gmail.com>
2022-03-28 16:29:36 -0400

    Add README.md, update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code here
+// import http module in node
+const http = require('http');
+
+// create server object that can send headers and body response
+const server = http.createServer((req, res) => {
+    // respond with status code of 200 (code for OK) as header
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    // log each GET (browser url) request path
+    console.log(`Received GET request for: ${req.url}`);
+    // respond requests with text as body
+    res.end("Hello World");
+  });
+  
+  // add listener so server can handle incoming connections (port, hostname, backlog, callback function)
+  server.listen(8080, 'localhost', () => {
+      // log connections
+      console.log('Server is listening on localhost:8080')
+  });
